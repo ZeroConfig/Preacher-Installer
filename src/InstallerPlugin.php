dee31b9f30573d3f0e629d057f6578f26f7544b0
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2017-02-18 14:17:43 +0100

    Initial setup

---
+++ src/InstallerPlugin.php
@@ -0,0 +1,29 @@
+<?php
+namespace ZeroConfig\Preacher\Installer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use ZeroConfig\Preacher\Environment;
+
+class InstallerPlugin implements PluginInterface
+{
+    /**
+     * Apply plugin modifications to Composer.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $inputOutput
+     *
+     * @return void
+     */
+    public function activate(Composer $composer, IOInterface $inputOutput)
+    {
+        $environment = new Environment();
+
+        $composer
+            ->getInstallationManager()
+            ->addInstaller(
+                new PluginInstaller($inputOutput, $composer, $environment)
+            );
+    }
+}
---
+++ src/PluginInstaller.php
@@ -0,0 +1,68 @@
+<?php
+namespace ZeroConfig\Preacher\Installer;
+
+use Composer\Composer;
+use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use ZeroConfig\Preacher\Environment;
+
+class PluginInstaller extends LibraryInstaller
+{
+    /** @var Environment */
+    private $environment;
+
+    /**
+     * Constructor.
+     *
+     * @param IOInterface $inputOutput
+     * @param Composer    $composer
+     * @param Environment $environment
+     */
+    public function __construct(
+        IOInterface $inputOutput,
+        Composer $composer,
+        Environment $environment
+    ) {
+        parent::__construct($inputOutput, $composer, 'preacher-plugin');
+        $this->environment = $environment;
+    }
+
+    /**
+     * Install the given plugin.
+     *
+     * @param InstalledRepositoryInterface $repo
+     * @param PackageInterface             $package
+     *
+     * @return void
+     */
+    public function install(
+        InstalledRepositoryInterface $repo,
+        PackageInterface $package
+    ) {
+        parent::install($repo, $package);
+        $class = $this->getBundleClass($package);
+    }
+
+    /**
+     * Remove the given plugin.
+     *
+     * @param InstalledRepositoryInterface $repo
+     * @param PackageInterface             $package
+     *
+     * @return void
+     */
+    public function uninstall(
+        InstalledRepositoryInterface $repo,
+        PackageInterface $package
+    ) {
+        $class = $this->getBundleClass($package);
+        parent::uninstall($repo, $package);
+    }
+
+    private function getBundleClass(PackageInterface $package): string
+    {
+        ddd($package->getTargetDir(), $package->getAutoload());
+    }
+}
